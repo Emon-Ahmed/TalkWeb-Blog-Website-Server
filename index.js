04a6907bdee4f0aa88c0fab31886975a75983750
Emon Ahmed <technicalboyemon@gmail.com>
2022-01-02 16:49:02 +0600

    CODE

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uana9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    console.log("Server and Database connection succesfully!!");
+  } finally {
+    // await client.close(); }
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
